2035fd01d8ce24a583bab964906cc1fe33ae830f
Mari-Ann Meling <mariann013@gmail.com>
2015-11-25 15:19:55 +0000

    can only rate from 1 to 5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,8 +4,14 @@ describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 
   it 'is not valid with a name of less that 3 charactars' do
-    p restaurant = Restaurant.new(name: 'kf')
+    restaurant = Restaurant.new(name: 'kf')
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,6 +1,8 @@
+require 'rails_helper'
+
 describe Review, type: :model do
   it { is_expected.to belong_to :restaurant }
-  
+
   let!(:kfc) {Restaurant.create(name: 'KFC')}
 
   it 'should remove the review when the restaurant is deleted' do
@@ -8,5 +10,10 @@ describe Review, type: :model do
     r.save!
     kfc.reviews << r
     expect{kfc.destroy}.to change {Review.count}.by(-1)
+  end
+
+  it 'is invalid if the rating is more that 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
   end
 end
